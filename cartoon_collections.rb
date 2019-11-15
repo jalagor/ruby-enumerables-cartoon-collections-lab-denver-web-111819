3c3c7b791c4bdd3cf1264d6759aacee1437373cf
John Rogala <johnrossrogala@gmail.com>
2019-11-15 13:43:51 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,6 @@
 def roll_call_dwarves# code an argument here
   # Your code here
-  array.each_with_index{|dwarf, index| puts "#{index + 1}. #{dwarf}"}
+  array.each_with_index{|name, index| puts "#{index + 1}. #{name}"}
 end
 
 def summon_captain_planet# code an argument here
