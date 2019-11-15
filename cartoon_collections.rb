11521e22c99c33d597e2be90132c7c59657cc296
John Rogala <johnrossrogala@gmail.com>
2019-11-15 13:50:50 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,6 +12,6 @@ def long_planeteer_calls(array)
 end
 
 def find_the_cheese(array)
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+cheese_types = ["cheddar", "gouda", "camembert"]
+array.find{|cheese| cheese == "cheddar" || cheese == "gouda" || cheese = "camembert"}
 end
